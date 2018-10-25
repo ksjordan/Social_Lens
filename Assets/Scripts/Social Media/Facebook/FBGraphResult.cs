06310d00e939bf9fd89d6ff603c3737dad925373
Christin Carter <christincarter95@gmail.com>
2018-10-24 23:34:00 -0700

    adding facebook scripts

---
+++ Assets/Scripts/Social Media/Facebook/FBGraphResult.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class FBGraphResult
+{
+    public List<FBPost> data;
+    public Paging paging;
+
+    public static FBGraphResult CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<FBGraphResult>(jsonString);
+    }
+}
+
+[System.Serializable]
+public class Paging
+{
+    public string previous;
+    public string next;
+
+    public static Paging CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<Paging>(jsonString);
+    }
+}
+
+[System.Serializable]
+public class FBPost
+{
+    public string id;
+    public string created_time;
+    public string message;
+    public string full_picture;
+    public string picture;
+
+    public static FBPost CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<FBPost>(jsonString);
+    }
+}
